491ea1bfa7b4c51a9eb2d4ce72c3913374298c5a
Chen Yu Pao <windperson@gmail.com>
2018-06-09 10:23:19 +0800

    1st implementation

--- TennisScoreCore/TennisGame.cs
+++ TennisScoreCore/TennisGame.cs
@@ -15,7 +15,72 @@ namespace TennisScoreCore
         {
             var game = this._repo.GetGame(gameId);
 
-            throw new NotImplementedException();
+            if (game.FirstPlayerScore == 0 && game.SecondPlayerScore == 0)
+            {
+                return "Love All";
+            }
+
+            if (game.FirstPlayerScore == 1 && game.SecondPlayerScore == 0)
+            {
+                return "Fifteen Love";
+            }
+            
+            if (game.FirstPlayerScore == 2 && game.SecondPlayerScore == 0)
+            {
+                return "Fifteen Love";
+            }
+            
+            if (game.FirstPlayerScore == 3 && game.SecondPlayerScore == 0)
+            {
+                return "Forty Love";
+            }
+            
+            if (game.FirstPlayerScore == 0 && game.SecondPlayerScore == 1)
+            {
+                return "Love Fifteen";
+            }
+            
+            if (game.FirstPlayerScore == 0 && game.SecondPlayerScore == 2)
+            {
+                return "Love Thirty";
+            }
+
+            if (game.FirstPlayerScore == 1 && game.SecondPlayerScore == 1)
+            {
+                return "Fifteen All";
+            }
+            
+            if (game.FirstPlayerScore == 2 && game.SecondPlayerScore == 2)
+            {
+                return "Thirty All";
+            }
+
+            if (game.FirstPlayerScore == 3 && game.SecondPlayerScore == 3)
+            {
+                return "Deuce";
+            }
+            
+            if (game.FirstPlayerScore == 4 && game.SecondPlayerScore == 3)
+            {
+                return "Joey Adv";
+            }
+            
+            if (game.FirstPlayerScore == 3 && game.SecondPlayerScore == 4)
+            {
+                return "Tom Adv";
+            }
+            
+            if (game.FirstPlayerScore == 4 && game.SecondPlayerScore == 4)
+            {
+                return "Deuce";
+            }
+
+            if (game.FirstPlayerScore == 5 && game.SecondPlayerScore == 3)
+            {
+                return "Joey Win";
+            }
+
+            return "";
         }
     }
 }
--- TennisScoreCore/UnitTest1.cs
+++ TennisScoreCore/UnitTest1.cs
@@ -19,5 +19,175 @@ namespace TennisScoreCore
             var scoreResult = tennisGame.ScoreResult(gameId);
             Assert.AreEqual("Love All", scoreResult);
         }
+        
+        [TestMethod]
+        public void Fifteen_Love()
+        {
+            var gameId = 1;
+
+            IRepository<Game> repo = Substitute.For<IRepository<Game>>();
+            repo.GetGame(gameId).Returns(new Game { Id = gameId, FirstPlayerScore = 1, SecondPlayerScore = 0 });
+
+            TennisGame tennisGame = new TennisGame(repo);
+
+            var scoreResult = tennisGame.ScoreResult(gameId);
+            Assert.AreEqual("Fifteen Love", scoreResult);
+        }
+        
+        [TestMethod]
+        public void Thirty_Love()
+        {
+            var gameId = 1;
+
+            IRepository<Game> repo = Substitute.For<IRepository<Game>>();
+            repo.GetGame(gameId).Returns(new Game { Id = gameId, FirstPlayerScore = 2, SecondPlayerScore = 0 });
+
+            TennisGame tennisGame = new TennisGame(repo);
+
+            var scoreResult = tennisGame.ScoreResult(gameId);
+            Assert.AreEqual("Fifteen Love", scoreResult);
+        }
+        
+        [TestMethod]
+        public void Fourty_Love()
+        {
+            var gameId = 1;
+
+            IRepository<Game> repo = Substitute.For<IRepository<Game>>();
+            repo.GetGame(gameId).Returns(new Game { Id = gameId, FirstPlayerScore = 3, SecondPlayerScore = 0 });
+
+            TennisGame tennisGame = new TennisGame(repo);
+
+            var scoreResult = tennisGame.ScoreResult(gameId);
+            Assert.AreEqual("Forty Love", scoreResult);
+        }
+        
+        [TestMethod]
+        public void Love_Fifteen()
+        {
+            var gameId = 1;
+
+            IRepository<Game> repo = Substitute.For<IRepository<Game>>();
+            repo.GetGame(gameId).Returns(new Game { Id = gameId, FirstPlayerScore = 0, SecondPlayerScore = 1 });
+
+            TennisGame tennisGame = new TennisGame(repo);
+
+            var scoreResult = tennisGame.ScoreResult(gameId);
+            Assert.AreEqual("Love Fifteen", scoreResult);
+        }
+        
+        [TestMethod]
+        public void Love_Thirty()
+        {
+            var gameId = 1;
+
+            IRepository<Game> repo = Substitute.For<IRepository<Game>>();
+            repo.GetGame(gameId).Returns(new Game { Id = gameId, FirstPlayerScore = 0, SecondPlayerScore = 2 });
+
+            TennisGame tennisGame = new TennisGame(repo);
+
+            var scoreResult = tennisGame.ScoreResult(gameId);
+            Assert.AreEqual("Love Thirty", scoreResult);
+        }
+        
+        [TestMethod]
+        public void Fifteen_All()
+        {
+            var gameId = 1;
+
+            IRepository<Game> repo = Substitute.For<IRepository<Game>>();
+            repo.GetGame(gameId).Returns(new Game { Id = gameId, FirstPlayerScore = 1, SecondPlayerScore = 1 });
+
+            TennisGame tennisGame = new TennisGame(repo);
+
+            var scoreResult = tennisGame.ScoreResult(gameId);
+            Assert.AreEqual("Fifteen All", scoreResult);
+        }
+        
+        [TestMethod]
+        public void Thirty_All()
+        {
+            var gameId = 1;
+
+            IRepository<Game> repo = Substitute.For<IRepository<Game>>();
+            repo.GetGame(gameId).Returns(new Game { Id = gameId, FirstPlayerScore = 2, SecondPlayerScore = 2 });
+
+            TennisGame tennisGame = new TennisGame(repo);
+
+            var scoreResult = tennisGame.ScoreResult(gameId);
+            Assert.AreEqual("Thirty All", scoreResult);
+        }
+
+        [TestMethod]
+        public void Deuce()
+        {
+            var gameId = 1;
+
+            IRepository<Game> repo = Substitute.For<IRepository<Game>>();
+            repo.GetGame(gameId).Returns(new Game { Id = gameId, FirstPlayerScore = 3, SecondPlayerScore = 3 });
+
+            TennisGame tennisGame = new TennisGame(repo);
+
+            var scoreResult = tennisGame.ScoreResult(gameId);
+            Assert.AreEqual("Deuce", scoreResult);
+        }
+        
+        [TestMethod]
+        public void Joey_Adv()
+        {
+            var gameId = 1;
+
+            IRepository<Game> repo = Substitute.For<IRepository<Game>>();
+            repo.GetGame(gameId).Returns(new Game { Id = gameId, FirstPlayerScore = 4, SecondPlayerScore = 3 });
+
+            TennisGame tennisGame = new TennisGame(repo);
+
+            var scoreResult = tennisGame.ScoreResult(gameId);
+            Assert.AreEqual("Joey Adv", scoreResult);
+        }
+        
+        [TestMethod]
+        public void Tom_Adv()
+        {
+            var gameId = 1;
+
+            IRepository<Game> repo = Substitute.For<IRepository<Game>>();
+            repo.GetGame(gameId).Returns(new Game { Id = gameId, FirstPlayerScore = 3, SecondPlayerScore = 4 });
+
+            TennisGame tennisGame = new TennisGame(repo);
+
+            var scoreResult = tennisGame.ScoreResult(gameId);
+            Assert.AreEqual("Tom Adv", scoreResult);
+        }
+        
+        [TestMethod]
+        public void Deuce_when4to4()
+        {
+            var gameId = 1;
+
+            IRepository<Game> repo = Substitute.For<IRepository<Game>>();
+            repo.GetGame(gameId).Returns(new Game { Id = gameId, FirstPlayerScore = 4, SecondPlayerScore = 4 });
+
+            TennisGame tennisGame = new TennisGame(repo);
+
+            var scoreResult = tennisGame.ScoreResult(gameId);
+            Assert.AreEqual("Deuce", scoreResult);
+        }
+
+        [TestMethod]
+        public void Joey_win()
+        {
+            var gameId = 1;
+
+            IRepository<Game> repo = Substitute.For<IRepository<Game>>();
+            repo.GetGame(gameId).Returns(new Game { Id = gameId, FirstPlayerScore = 5, SecondPlayerScore = 3 });
+
+            TennisGame tennisGame = new TennisGame(repo);
+
+            var scoreResult = tennisGame.ScoreResult(gameId);
+            Assert.AreEqual("Joey Win", scoreResult);
+        }
+
+        
     }
 }
